873e062ca44703f19ac14f89c0a2ba82b2791534
Guido <guido.esser@mailbox.org>
2021-05-14 12:26:48 +0200

    Playable

--- tic-tac-toe/tictactoe.py
+++ tic-tac-toe/tictactoe.py
@@ -1,37 +1,42 @@
 import pandas as pd
+import os
+
 df = pd.read_csv('init.csv')
-print(df)
 
-def get_cell(move):
-    if move == 1: return df.iloc[0][0]
-    if move == 2: return df.iloc[0][1]
-    if move == 3: return df.iloc[0][2]
-    if move == 4: return df.iloc[1][0]
-    if move == 5: return df.iloc[1][1]
-    if move == 6: return df.iloc[1][2]
-    if move == 7: return df.iloc[2][0]
-    if move == 8: return df.iloc[2][1]
-    if move == 9: return df.iloc[2][2]
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def set_cell(move, piece):
+def get_cell(my_move):
+    if my_move == 1: return df.iloc[0][0]
+    if my_move == 2: return df.iloc[0][1]
+    if my_move == 3: return df.iloc[0][2]
+    if my_move == 4: return df.iloc[1][0]
+    if my_move == 5: return df.iloc[1][1]
+    if my_move == 6: return df.iloc[1][2]
+    if my_move == 7: return df.iloc[2][0]
+    if my_move == 8: return df.iloc[2][1]
+    if my_move == 9: return df.iloc[2][2]
+
+
+def set_cell(my_move, my_piece):
     if get_cell(move) not in ('x', 'o'):
-        if move == 1: df.iloc[0][0] = piece
-        if move == 2: df.iloc[0][1] = piece
-        if move == 3: df.iloc[0][2] = piece
-        if move == 4: df.iloc[1][0] = piece
-        if move == 5: df.iloc[1][1] = piece
-        if move == 6: df.iloc[1][2] = piece
-        if move == 7: df.iloc[2][0] = piece
-        if move == 8: df.iloc[2][1] = piece
-        if move == 9: df.iloc[2][2] = piece
+        if my_move == 1: df.iloc[0][0] = my_piece
+        if my_move == 2: df.iloc[0][1] = my_piece
+        if my_move == 3: df.iloc[0][2] = my_piece
+        if my_move == 4: df.iloc[1][0] = my_piece
+        if my_move == 5: df.iloc[1][1] = my_piece
+        if my_move == 6: df.iloc[1][2] = my_piece
+        if my_move == 7: df.iloc[2][0] = my_piece
+        if my_move == 8: df.iloc[2][1] = my_piece
+        if my_move == 9: df.iloc[2][2] = my_piece
         return True
     else:
         return False
 
 
 def has_won():
-    row1 = get_cell(1) + get_cell(1) + get_cell(3)
+    row1 = get_cell(1) + get_cell(2) + get_cell(3)
     if row1 in ('xxx', 'ooo'): return True
     row2 = get_cell(4) + get_cell(5) + get_cell(6)
     if row2 in ('xxx', 'ooo'): return True
@@ -54,9 +59,7 @@ def has_won():
 user1 = 'Player 1'  # input("Enter Name1: ")
 user2 = 'Player 2'  # input("Enter Name1: ")
 active_user = user1
-
-won = has_won()
-print(won)
+all_moves = [i for i in range(1, 10)]
 
 while not has_won():
     # Main
@@ -64,13 +67,41 @@ while not has_won():
         piece = 'x'
     else:
         piece = 'o'
-    
-    move = int(input("Enter move: "))
-    if set_cell(move, piece):
-        print("/n")
-        print(df)
 
-print("Sie haben gewonnen!")
+    print(df)
+
+    try:
+        move = int(input("Enter move: "))
+
+    except:
+        print("This move is not possible, please try again")
+        print(all_moves)
+
+    if set_cell(move, piece):
+        print("Move successful.")
+        all_moves.remove(move)
+
+        if has_won():
+            print("Sie haben gewonnen!")
+            print(df)
+            break
+
+        elif not all_moves:
+            print("Unentschieden!")
+            break
+
+        if active_user == user1:
+            active_user = user2
+        else:
+            active_user = user1
+
+    else:
+        print("This move is not possible, please try again")
+        print(all_moves)
+
+
+
+
 
     
 
